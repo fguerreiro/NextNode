692de709534c6fdb76f75efa36de3d62524ddef0
Francisco Guerreiro <fguerreiro@absolunet.com>
2019-07-29 16:43:51 -0400

    Fixed tests and cleanups

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ConsoleApp1
 {
@@ -102,41 +103,22 @@ namespace ConsoleApp1
 
     public static class NodeExtensions
     {
-        private static Node[] PreOrderedArray { get; set; }
-
-        public static Node[] ToPreOrderedArray(this Node n)
-        {
-            // For performance, cache tree as pre-ordered array once, when traversing from root; otherwise return cached array. 
-            if (n.IsRoot())
-            {
-                var visited = new List<Node>();
-                n.VisitNodes(visited);
-
-                PreOrderedArray = visited.ToArray();
-            }
-
-            return PreOrderedArray;
-        }
-
-        public static void VisitNodes(this Node n, List<Node> visited)
-        {
-            visited.Add(n);
-
-            foreach (var nonVisitedChild in n.UnvisitedChildren(visited))
-            {
-                VisitNodes(nonVisitedChild, visited);
-            }
-        }
-
         public static Node Next(this Node n)
         {
             var preOrdered = n.ToPreOrderedArray();
-            var currentIndex = Array.IndexOf(preOrdered, n);
+            var currentNodeIndex = Array.IndexOf(preOrdered, n);
 
-            var upperBoundNextElement = currentIndex + 1;
+            var upperBoundNextElement = currentNodeIndex + 1;
             Node next = preOrdered.Length > upperBoundNextElement ? preOrdered[upperBoundNextElement] : null;
 
             return next;
+        }
+
+        public static string VisitAllNodes(this Node root)
+        {
+            var visited = root.ToPreOrderedArray().ToList();
+
+            return string.Join(",", visited);
         }
 
         public static bool IsRoot(this Node n)
@@ -144,9 +126,114 @@ namespace ConsoleApp1
             return n.Parent == null;
         }
 
-        public static IEnumerable<Node> UnvisitedChildren(this Node parent, IEnumerable<Node> visited)
+        private static IEnumerable<Node> UnvisitedChildren(this Node parent, IEnumerable<Node> visited)
         {
             return parent.Children.Except(visited);
         }
+
+        private static void VisitNodes(this Node currentNode, List<Node> visited)
+        {
+            visited.Add(currentNode);
+
+            foreach (var nonVisitedChild in currentNode.UnvisitedChildren(visited))
+            {
+                VisitNodes(nonVisitedChild, visited);
+            }
+        }
+
+        private static Node[] ToPreOrderedArray(this Node n)
+        {
+            // For performance, store cache tree as pre-ordered array once, when traversing from root; otherwise return cached array. 
+            if (n.IsRoot())
+            {
+                var visited = new List<Node>();
+                n.VisitNodes(visited);
+
+                _preOrderedTreeCache = visited.ToArray();
+            }
+
+            return _preOrderedTreeCache;
+        }
+
+        private static Node[] _preOrderedTreeCache { get; set; } = new Node[0];
+    }
+
+    [TestFixture]
+    class NodeTests
+    {
+        public Node Tree => new Node(
+            1,
+            new Node(
+                2,
+                new Node(3),
+                new Node(4)),
+            new Node(
+                5,
+                new Node(6),
+                new Node(7)));
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        public void NodeGetsNextChild(int nodeIndex)
+        {
+            var root = Tree;
+
+            var n = root;
+            var currentData = 0;
+            var currentIndex = 1;
+            while (n != null && currentIndex++ <= nodeIndex)
+            {
+                currentData = n.Data;
+                TestContext.WriteLine($"Current node: {currentData}");
+                n = n.Next();
+            }
+            Assert.AreEqual(nodeIndex, currentData);
+        }
+
+        [Test]
+        public void UnbalanceTree()
+        {
+            var unbalancedTree = 
+                new Node(1, 
+                    new Node(2),
+                new Node(3),
+                new Node(4,
+                    new Node(5,
+                        new Node(6))),
+                new Node(7,
+                    new Node(8), 
+                    new Node(9)));
+
+            var visited = unbalancedTree.VisitAllNodes();
+
+            Assert.AreEqual("1,2,3,4,5,6,7,8,9", visited);
+        }
+
+        [Test]
+        public void UnbalanceTree2()
+        {
+            var unbalancedTree =
+                new Node(1,
+                    new Node(2),
+                    new Node(3,
+                        new Node(4)),
+                    new Node(5,
+                        new Node(6),
+                        new Node(7,
+                            new Node(8))),
+                    new Node(9,
+                        new Node(10)));
+
+            var visited = unbalancedTree.VisitAllNodes();
+
+            Assert.AreEqual("1,2,3,4,5,6,7,8,9,10", visited);
+        }
+
+
     }
 }
